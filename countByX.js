2f9a574a027766faf450afbf6f15cb3cad0a78ed
AhmedInes <ahmed.bouzid.ines@gmail.com>
2023-07-22 16:34:56 +0200

    countByX

--- countByX.js
+++ countByX.js
@@ -2,6 +2,19 @@
 
 
 
-    function countByX (){
+    function countByX (x , n){
+/* let tab = [] ;
+        for (let index = 1; index <= n; index++) {
+            
+        tab.push(x * index)     
+       }
+        return tab */
+
         
+        let arr = Array.from(Array(n+1).keys())
+        .slice(1)
+        .map((num)=> num * x)
+        return arr
     }
+
+    console.log(countByX(1 , 10));
