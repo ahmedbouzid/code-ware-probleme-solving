7a5d39fd080bda2950875e97f0fdde44992ce490
AhmedInes <ahmed.bouzid.ines@gmail.com>
2023-07-23 11:34:32 +0200

    ex

--- areYouplayingPanjo.js
+++ areYouplayingPanjo.js
@@ -3,8 +3,17 @@
 
     function arePlaying(name) {
 
-         
+         //// check the coming name 
+         /// if the name start with R or R return play banjo
+         /// if not return dosent play
+/*          if (name.charAt(0) === 'r' || name.charAt(0) === 'R'){
+            return name + 'play banjo'
+         }
+         else { 
+           return name +  ' dosnet play'
+         } */
+         return name[0] === 'r' || name[0] === 'R' ? name + 'play' : 'does not play'
 
     }
 
-    console.log(arePlaying());
+    console.log(arePlaying('Rami '));
