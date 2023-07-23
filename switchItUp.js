13a3e0044abf90fc4e5558c308583713b877685b
AhmedInes <ahmed.bouzid.ines@gmail.com>
2023-07-23 10:14:56 +0200

    switch number

---
+++ switchItUp.js
@@ -0,0 +1,41 @@
+
+
+
+    function switchItUp(number){
+
+
+        switch (number) {
+            case 1:
+                return 'one'
+                break;
+            case 2 : 
+            return 'two'
+            break ;           
+            case 3 : 
+            return 'three'
+            break ;
+            case 4 : 
+            return 'four'
+            break ;
+            case 5 : 
+            return 'five'
+            break ;
+            case  6: 
+            return 'six'
+            break ;
+            case 7 : 
+            return 'seven'
+            break ;
+            case 8 : 
+            return 'nine'
+            break ;           
+             case 9 : 
+            return 'eight'
+            break ;
+        
+            default:
+                return 'wrong numb'
+                break;
+        }
+    }
+    console.log(switchItUp(50));
